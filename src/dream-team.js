76cb73beceb2ea050084b995bbb004c32a6a8f45
SovanMarat <maratwp@mail.ru>
2020-10-12 15:54:47 +0300

    feat: function createDreamTeam done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+    }
+  if (members.length === 0) {
+    return false;
+  }
+  let nameTeam = members.map(function (item) {
+    let typeItem = typeof (item);
+    if (typeItem === 'string') {
+      item = item.replace(/\s/g, '');
+      return item[0].toUpperCase();
+    }
+  });
+  nameTeam=nameTeam.sort();
+  nameTeam = nameTeam.join('');
+  return nameTeam;
+}
