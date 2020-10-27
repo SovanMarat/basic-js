fcda7960b6499b81eefff60b2a4da95a3faacce8
SovanMarat <maratwp@mail.ru>
2020-10-27 23:41:09 +0300

    feat: function getSeason edit, function calculateHanoi done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,30 +1,6 @@
-// const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error");
 
-// module.exports = function transform(arr) {
-//      let arrNew=[];
-//      for (i=0; i<arr.length; i++) {
-// switch (arr[i]) {
-//     case "--discard-next": {
-//     i++;
-//     break;
-// }
-// case "--discard-prev": {
-//     arrNew.pop();
-//     break;
-// }
-// case "--double-next": {
-//     arrNew.push(arr[i+1]*2);
-//     i++;
-//     break;
-// }
-// case "--double-prev": {
-//     arrNew.pop();
-//     arrNew.push(arr[i-1]*2);
-//     break;
-// }
-// default: arrNew.push(arr[i]);
-// }
-//      };
-//      console.log(arrNew);
-//      return arrNew;
-// };
+module.exports = function transform(/* arr */) {
+  throw new CustomError('Not implemented');
+  // remove line with error and write your code here
+};
--- src/what-season.js
+++ src/what-season.js
@@ -1,11 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
- module.exports = function getSeason(date) {
+module.exports = function getSeason(date) {
+  console.log('-----------------------');
   console.log(date);
-  if (String(date)==="Invalid Date"){
-    console.log('ошибка');
-    throw Error;
+  console.log('-----------------------');
+  let toString = {}.toString;
+  if (date==null) {
+    console.log('11111');
+    return 'Unable to determine the time of year!';
   }
+   if (toString.call(date)!=='[object Date]'){
+      console.log('jijjjj');
+     throw Error;
+    }
   let month = date.getMonth();
   if (month===11 || month===00 || month===01) {
     console.log('winter');
@@ -27,7 +34,7 @@ const CustomError = require("../extensions/custom-error");
 
   // console.log(date);
 };
-// let date = new Date("aaa", 01, 01, 18, 36, 41, 841);
+//  let date = new Date("aaa", 01, 01, 18, 36, 41, 841);
 // getSeason(date);
 
 // spring, summer, autumn (fall), .
