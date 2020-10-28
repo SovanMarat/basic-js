367d3f0cf7d6d533a13f7e0496efa301eca617a7
SovanMarat <maratwp@mail.ru>
2020-10-28 22:08:27 +0300

    feat: function transform done

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  // console.log('---------------');
+  // console.log(arr);
+  // console.log('---------------');
+  if (!Array.isArray(arr)) {
+    throw Error;
+    }
+    let arrNew=[];
+    for (i=0; i<arr.length; i++) {
+    switch (arr[i]) {
+    case "--discard-next": {
+    if (i<arr.length-1) {
+    i++;
+    }
+    break;
+    }
+    case "--discard-prev": {
+      if (arr[i-2]==="--discard-next"){
+        break;
+      }
+    if (i>0) {
+    arrNew.pop();
+    }
+    break;
+  }
+    case "--double-next": {
+    if (i<arr.length-1) {
+    arrNew.push(arr[i+1]);
+    }
+    break;
+    }
+    case "--double-prev": {
+      if (arr[i-2]==="--discard-next"){
+        break;
+      }
+    if (i>0) {
+    arrNew.push(arr[i-1]);
+    }
+    break;
+    }
+    default: arrNew.push(arr[i]);
+    }
+    }
+    console.log(arrNew);
+    return arrNew;
+    };
+
+    //transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5],);
