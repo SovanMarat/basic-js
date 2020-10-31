2dc1c4054056b4f873971bd6371faf2370a1d950
SovanMarat <maratwp@mail.ru>
2020-10-31 12:53:32 +0300

    feat: chainMaker done

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,61 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  arr:[],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   //this.arr.push(this.arr.length); если выводить  длину в цепочку
+   this.arr.length; 
+   return this;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+ addLink(value) {
+   if (String(value)==='undefined') {
+    this.arr.push(' ');
+   } else {
+this.arr.push(value);
+}
+return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position<1  || position>this.arr.length-1   || typeof(position)!=='number' || position!==Math.ceil(position)){
+     // return false;
+  //console.log('erorr');
+  this.arr.length=0;
+      throw Error;
+    }
+this.arr.splice(position-1,1);
+return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+this.arr.reverse();
+return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  let result='';
+  let aaa='';
+//console.log(this.arr);
+result=this.arr;
+//console.log(result);
+
+for (let i=0; i<result.length; i++){
+if (i===result.length-1){
+  aaa+=`( ${result[i]} )`;
+  //console.log(aaa);
+ // console.log(`aaa=${aaa}`);
+  //return aaa;
+} else {
+aaa+=`( ${result[i]} )~~`;
+}
+}
+//console.log(aaa);
+this.arr.length=0;
+////console.log(aaa);
+//console.log(this.arr.length);
+return aaa;
   }
 };
 
 module.exports = chainMaker;
+
+ //chainMaker.addLink(0).addLink(1).addLink(true).addLink(3).removeLink(2).reverseChain().finishChain();
+//  console.log(chainMaker.arr);
